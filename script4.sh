5795b2c9ce347e1363d15c0a7dd52524bc6aae5c
root <root@ip-172-31-28-254.ec2.internal>
2022-09-19 21:38:35 +0000

    ai

--- script4.sh
+++ script4.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 aws ec2 run-instances --image-id ami-031eec5728455829f --count 1 --instance-type t2.micro --key-name m1 --region us-east-1
 
-aws ec2 terminate-instances --instances-id i-01b10f4e1d4accbfe --profile i1
