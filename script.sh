5d30e82fbfa21370cc1fd27bba954eee46fbfa37
root <root@ip-172-31-28-254.ec2.internal>
2022-09-19 20:20:38 +0000

    first

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 aws ec2 run-instances --image-id ami-05fa00d4c63e32376 --count 1 --instance-type t2.micro --key-name m1 --region us-east-1
 
-sh "aws ec2 create-image --instance-id i-01ce56cc4081c9bdf --name 'machine1' --region us-east-1 --no-reboot"
