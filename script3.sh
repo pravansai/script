ed60ae4b22b4f7028ae8af17209b992482888f9e
root <root@ip-172-31-28-254.ec2.internal>
2022-09-19 20:59:42 +0000

    third

--- script3.sh
+++ script3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d_%H-%M) 
-AMI_NAME="Wordpress Backup - $DATE"
-AMI_DESCRIPTION="Wordpress Backup - $DATE"
+AMI_NAME="Image Backup - $DATE"
+AMI_DESCRIPTION="Image Backup - $DATE"
 INSTANCE_ID=i-01b10f4e1d4accbfe
 
 printf "Requesting AMI for instance i-01b10f4e1d4accbfe...\n"
