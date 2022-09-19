2501530e136eff3d3614920b6e73a3d84a936840
root <root@ip-172-31-28-254.ec2.internal>
2022-09-19 20:43:46 +0000

    third

--- script2.sh
+++ script2.sh
@@ -2,10 +2,10 @@
 DATE=$(date +%Y-%m-%d_%H-%M) 
 AMI_NAME="Wordpress Backup - $DATE"
 AMI_DESCRIPTION="Wordpress Backup - $DATE"
-INSTANCE_ID=$1
+INSTANCE_ID=i-01ce56cc4081c9bdf
 
-printf "Requesting AMI for instance $1...\n"
-aws ec2 create-image --instance-id $1 --name "$AMI_NAME" --description "$AMI_DESCRIPTION" --no-reboot
+printf "Requesting AMI for instance i-01ce56cc4081c9bdf...\n"
+aws ec2 create-image --instance-id i-01ce56cc4081c9bdf --name "$AMI_NAME" --description "$AMI_DESCRIPTION" --no-reboot
 
 if [ $? -eq 0 ]; then
 	printf "AMI request complete!\n"
